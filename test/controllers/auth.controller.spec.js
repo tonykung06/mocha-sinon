2540812df08e1c4839f64b02dd0b25214a84e78b
tonykung <tonykung06@gmail.com>
2017-08-22 00:39:12 +0800

    using chai expect

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert')
 const AuthControllerFactory = require('../../controllers/auth.controller')
+const expect = require('chai').expect
 
 beforeEach(function globalSetup () {
   console.log('global beforeEach is running for every unit test functions across all test files')
@@ -17,11 +17,11 @@ describe('AuthController', () => {
   // this.skip
   describe('isAuthorized', () => {
     it('returns false if not authorized', () => {
-      assert.equal(false, authController.isAuthorized('admin'))
+      expect(authController.isAuthorized('admin')).to.be.false
     })
     it('returns true if authorized', () => {
       authController.setRoles(['user', 'admin'])
-      assert.equal(true, authController.isAuthorized('admin'))
+      expect(authController.isAuthorized('admin')).to.be.true
     })
     it('does NOT allow a get if not authorized')
     it('allows get if authorized')
@@ -31,13 +31,13 @@ describe('AuthController', () => {
     it('returns false if not authorized', async function () {
       this.timeout(2500)
       const result = await authController.isAuthorizedAsync('admin')
-      assert.equal(false, result)
+      expect(result).to.be.false
     })
     it('returns true if authorized', async function () {
       this.timeout(2500)
       authController.setRoles(['user', 'admin'])
       const result = await authController.isAuthorizedAsync('admin')
-      assert.equal(true, result)
+      expect(result).to.be.true
     })
   })
 })
--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,7 +1,21 @@
-const assert = require('assert')
+const expect = require('chai').expect
 
 describe('Basic Mocha Test', () => {
-    it('passes', () => {
-        assert.equal(3, 3)
-    })
+  it('deals with objects', () => {
+    const obj = {
+      name: 'tony',
+      gender: 'male'
+    }
+    const obj2 = {
+      name: 'tony',
+      gender: 'male'
+    }
+    expect(obj).to.have.property('name').equal('tony')
+    expect(obj).to.deep.equal(obj2)
+  })
+
+  it('deals with null', () => {
+    var myNull = null
+    expect(myNull).to.not.exist
+  })
 })
